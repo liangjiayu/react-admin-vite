4a66d892e9bb12d25217010058c9e76e60e84e88
liangjiayu <1013834609@qq.com>
2025-06-22 12:14:31 +0800

    init: 添加框架核心依赖

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import { Button } from "antd";
+
+const Home = () => {
+  return (
+    <div className="p-[20px]">
+      <Button type="primary">Primary Button</Button>
+    </div>
+  );
+};
+
+export default Home;
