e1b910b5f197487b56bdbc8434f92520f02965c7
liangjiayu <1013834609@qq.com>
2025-06-23 21:58:14 +0800

    init: 菜单功能定义

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,8 +1,34 @@
+import { CrownFilled, SmileFilled } from "@ant-design/icons";
 import { ProLayout } from "@ant-design/pro-components";
-import { Outlet } from "react-router";
+import { Link, Outlet, useLocation } from "react-router";
 
 const BaseLayout = () => {
-  return <ProLayout children={<Outlet />} token={{ bgLayout: "#f2f2f2" }} />;
+  const location = useLocation();
+
+  return (
+    <ProLayout
+      children={<Outlet />}
+      token={{ bgLayout: "#f2f2f2" }}
+      location={location}
+      menuItemRender={(item, dom) => <Link to={item.path}>{dom}</Link>}
+      menuDataRender={() => {
+        return [
+          {
+            path: "/welcome",
+            name: "欢迎",
+            icon: <SmileFilled />,
+            // component: "./Welcome",
+          },
+          {
+            path: "/table-list",
+            name: "管理页",
+            icon: <CrownFilled />,
+            // component: "./Admin",
+          },
+        ];
+      }}
+    />
+  );
 };
 
 export default BaseLayout;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router";
 import Home from "../pages/home";
 import TableList from "../pages/table-list";
 import BaseLayout from "../layout";
+import Welcome from "@/pages/welcome";
 
 const router = createBrowserRouter([
   {
@@ -9,6 +10,7 @@ const router = createBrowserRouter([
     children: [
       { index: true, Component: Home },
       { path: "/table-list", Component: TableList },
+      { path: "/welcome", Component: Welcome },
     ],
   },
 ]);
