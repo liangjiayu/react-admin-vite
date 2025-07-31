db96322122fba83e42cdf249d187f17f256b2d3d
liangjiayu <1013834609@qq.com>
2025-07-31 22:32:06 +0800

    feat: 添加Question组件及外部链接功能

---
+++ src/layout/widgets/right-content/question.tsx
@@ -0,0 +1,19 @@
+import { QuestionCircleOutlined } from '@ant-design/icons';
+
+const Question = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        height: 26,
+      }}
+      onClick={() => {
+        window.open('https://pro.ant.design/docs/getting-started');
+      }}
+    >
+      <QuestionCircleOutlined />
+    </div>
+  );
+};
+
+export default Question;
