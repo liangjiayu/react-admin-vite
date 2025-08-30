85055127ea9e18e9df790fae1857ef98051ed4a5
liangjiayu <1013834609@qq.com>
2025-08-30 14:12:25 +0800

    chore: 切换回标准vite和react插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import process from 'node:process';
 import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react-oxc';
+import react from '@vitejs/plugin-react';
 import dayjs from 'dayjs';
 import { defineConfig, loadEnv } from 'vite';
 import { mockDevServerPlugin } from 'vite-plugin-mock-dev-server';
